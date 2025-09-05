b6c5c075eca478ff64d94149c0575cff3dd378ec
Cruzbuddy41 <bd568711@ahschool.com>
2025-09-05 08:00:33 -0400

    Some basic functions to use

--- Code/defOfFunctions.py
+++ Code/defOfFunctions.py
@@ -1,1 +1,8 @@
+def turnLeft(power):
+    """This function is a place holder for the code for the robot to turn left"""
 
+def turnRight(power):
+    """This function is a place holder for the code for the robot to turn right"""
+
+def moveForward(power):
+    """This function is a place holder for the code for the robot to move forward"""
