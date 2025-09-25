b9cf6e3959b0a52d8fa524d5020494e08d7d63ae
Cruzbuddy41 <bd568711@ahschool.com>
2025-09-25 08:06:57 -0400

    Delete Code/defOfFunctions.py

--- Code/defOfFunctions.py
+++
@@ -1,8 +0,0 @@
-def turnLeft(power):
-    """This function is a place holder for the code for the robot to turn left"""
-
-def turnRight(power):
-    """This function is a place holder for the code for the robot to turn right"""
-
-def moveForward(power):
-    """This function is a place holder for the code for the robot to move forward"""
