fc0c45d1972731e553cded25e8d835954460ac2d
deadcyberbs <bd6010870@ahschool.com>
2025-09-29 08:42:21 -0400

    Update Testfwfile.py

--- Code/Micah/Testfwfile.py
+++ Code/Micah/Testfwfile.py
@@ -2,14 +2,14 @@ import gpiozero as GPIO
 import time
 from classes_we_use import HR8825
 
+Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+
+Motor1.SetMicroStep('hardward','1/16step')
+Motor2.SetMicroStep('hardward','1/16step')
+
 def forward(steps):
     try:
-        Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
-        Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
-		
-        Motor1.SetMicroStep('hardward','1/16step')
-        Motor2.SetMicroStep('hardward' ,'1/16step')
-		
         for i in range(steps):
             Motor2.TurnStep(Dir='backward', steps=1, stepdelay=0.001)
             Motor1.TurnStep(Dir='forward', steps=1, stepdelay = 0.001)
