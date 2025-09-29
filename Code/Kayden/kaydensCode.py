01c0aa46c74e1140870663ffd4e2d4639a043809
Cruzbuddy41 <bd568711@ahschool.com>
2025-09-29 07:46:25 -0400

    Update kaydensCode.py

--- Code/Kayden/kaydensCode.py
+++ Code/Kayden/kaydensCode.py
@@ -2,26 +2,25 @@ import gpiozero as GPIO
 import time
 from classes_we_use import HR8825
 
-def foward(input):
-	for i in range(input):
-		Motor2.TurnStep(Dir='forward', steps=1)	
-		Motor1.TurnStep(Dir='forward', steps=1)
-try:
-	Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
-	Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+def forward(steps):
+	try:
+		Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+		Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
-	Motor1.SetMicroStep('hardward','halfstep')
-	Motor2.SetMicroStep('hardward','halfstep')
-	forward(1000)
-	time.sleep(0.5)
-	Motor1.Stop()
-	Motor2.Stop()
-
-	Motor1.Stop()
-	Motor2.Stop()
+		Motor1.SetMicroStep('hardward','1/16step')
+		Motor2.SetMicroStep('hardward' ,'1/16step')
+ 		a = steps
+  		while a != 0:
+			Motor2.TurnStep(Dir='backward', steps=1, stepdelay=0.0005)
+			Motor1.TurnStep(Dir='forward', steps=1, stepdelay = 0.0005)
+			a = a - 1
+		Motor1.Stop()
+		Motor2.Stop()
     
-except:
-    print("\nMotor stop")
-    Motor1.Stop()
-    Motor2.Stop()
-    exit()
+	except:
+		print("\nMotor stop")
+		Motor1.Stop()
+		Motor2.Stop()
+		exit()
+
+forward(2048)
