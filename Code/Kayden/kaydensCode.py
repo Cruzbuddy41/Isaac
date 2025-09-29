463b24257d10f24d15d211a2b715921bf66ed77f
Cruzbuddy41 <bd568711@ahschool.com>
2025-09-29 08:24:26 -0400

    Update kaydensCode.py

--- Code/Kayden/kaydensCode.py
+++ Code/Kayden/kaydensCode.py
@@ -8,10 +8,8 @@ def forward(steps):
 		Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
 		Motor1.SetMicroStep('hardward','1/16step')
-		Motor2.SetMicroStep('hardward' ,'1/16step')
  		a = steps
   		while a != 0:
-			Motor2.TurnStep(Dir='backward', steps=1, stepdelay=0.0005)
 			Motor1.TurnStep(Dir='forward', steps=1, stepdelay = 0.0005)
 			a = a - 1
 		Motor1.Stop()
