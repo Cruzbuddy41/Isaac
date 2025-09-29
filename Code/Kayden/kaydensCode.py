64a77a35062dfede57b2866761bd462399b2a223
Cruzbuddy41 <bd568711@ahschool.com>
2025-09-29 08:31:19 -0400

    Update kaydensCode.py

--- Code/Kayden/kaydensCode.py
+++ Code/Kayden/kaydensCode.py
@@ -3,22 +3,23 @@ import time
 from classes_we_use import HR8825
 
 def forward(steps):
-	try:
-		Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
-		Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+    try:
+        Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+        Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
-		Motor1.SetMicroStep('hardward','1/16step')
- 		a = steps
-  		while a != 0:
-			Motor1.TurnStep(Dir='forward', steps=1, stepdelay = 0.0005)
-			a = a - 1
-		Motor1.Stop()
-		Motor2.Stop()
-    
-	except:
-		print("\nMotor stop")
-		Motor1.Stop()
-		Motor2.Stop()
-		exit()
+        Motor1.SetMicroStep('hardward','1/16step')
+        a = steps
+        while a != 0:
+            Motor1.TurnStep(Dir='forward', steps=1, stepdelay = 0.0005)
+            a = a - 1
+        Motor1.Stop()
+        Motor2.Stop()
+		
+	
+    except:
+        print("\nMotor stop")
+        Motor1.Stop()
+        Motor2.Stop()
+        exit()
 
 forward(400)
