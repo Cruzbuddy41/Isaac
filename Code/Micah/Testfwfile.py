240fc19d7030b557a96c5e4ad86994b63a393b26
deadcyberbs <bd6010870@ahschool.com>
2025-10-03 08:05:07 -0400

    hello

--- Code/Micah/Testfwfile.py
+++ Code/Micah/Testfwfile.py
@@ -9,7 +9,7 @@ def forward(steps):
         Motor1.SetMicroStep('hardward', '1/16step')
         Motor2.SetMicroStep('hardward', '1/16step')
         print("Motors running. Press Ctrl+C to stop.")
-        time.sleep(1) # hello
+        time.sleep(2.5) # hello
         for i in range(steps):
             Motor2.TurnStep(Dir='forward', steps=1, stepdelay=0.05)
             Motor1.TurnStep(Dir='backward', steps=1, stepdelay=0.05)
@@ -19,4 +19,4 @@ def forward(steps):
         Motor1.Stop()
         Motor2.Stop()
         print("Motors stopped.")
-forward(15)
+forward(60)
