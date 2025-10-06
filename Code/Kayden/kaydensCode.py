9805c823621aaf271d669822e17731043c65c103
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-06 07:46:44 -0400

    Update kaydensCode.py

--- Code/Kayden/kaydensCode.py
+++ Code/Kayden/kaydensCode.py
@@ -9,14 +9,38 @@ def forward(steps):
         Motor1.SetMicroStep('hardward', '1/16step')
         Motor2.SetMicroStep('hardward', '1/16step')
         print("Motors running. Press Ctrl+C to stop.")
-        time.sleep(5) # hello
-        for i in range(steps):
-            Motor2.TurnStep(Dir='forward', steps=1, stepdelay=0.05)
-            Motor1.TurnStep(Dir='backward', steps=1, stepdelay=0.05)
+        time.sleep(2.5)
+        for i in range(0, steps):
+            Motor2.Turn(Dir='forward')
+            Motor1.Turn(Dir='backward')
+            time.sleep(0.05)
+            Motor2.Stop()
+            Motor1.Stop()
     except KeyboardInterrupt:
         print("\nStopping motors gracefully...")
     finally:
         Motor1.Stop()
         Motor2.Stop()
         print("Motors stopped.")
-forward(30)
+def backward(steps):
+    Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+    Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+    try:
+        Motor1.SetMicroStep('hardward', '1/16step')
+        Motor2.SetMicroStep('hardward', '1/16step')
+        print("Motors running. Press Ctrl+C to stop.")
+        time.sleep(2.5)
+        for i in range(0, steps):
+            Motor2.Turn(Dir='backward')
+            Motor1.Turn(Dir='forward')
+            time.sleep(0.05)
+            Motor2.Stop()
+            Motor1.Stop()
+    except KeyboardInterrupt:
+        print("\nStopping motors gracefully...")
+    finally:
+        Motor1.Stop()
+        Motor2.Stop()
+        print("Motors stopped.")
+forward(40)
+backward(40)
