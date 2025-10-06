adceeb3d6b228beaa2f1b73657887f44e56d900a
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-06 08:38:19 -0400

    Update Testfwfile.py

--- Code/Micah/Testfwfile.py
+++ Code/Micah/Testfwfile.py
@@ -1,7 +1,6 @@
 import gpiozero as GPIO
 import time
 from classes_we_use import HR8825
-time.sleep(2)
 def forward(steps):
     Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
     Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
@@ -9,11 +8,11 @@ def forward(steps):
         Motor1.SetMicroStep('hardward', '1/16step')
         Motor2.SetMicroStep('hardward', '1/16step')
         print("Motors running. Press Ctrl+C to stop.")
-        time.sleep(5)
+        time.sleep(2.5)
         for i in range(steps):
-            Motor2.Turn(Dir='forward')
-            Motor1.Turn(Dir='backward')
-            time.sleep(0.1)
+            Motor1.Turn(Dir='forward')
+            Motor2.Turn(Dir='backward')
+            time.sleep(0.05)
             Motor2.Stop()
             Motor1.Stop()
     except KeyboardInterrupt:
