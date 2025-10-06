a9e2557be1dc17eaf82ea3fa4ac8629c9e92cf24
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-06 08:20:58 -0400

    Update classes_we_use.py

--- Code/Kayden/classes_we_use.py
+++ Code/Kayden/classes_we_use.py
@@ -95,12 +95,20 @@ class HR8825():
             time.sleep(stepdelay)
 
     def Turn(self, Dir):
-        if (Dir == "forward"):
+        if (Dir == MotorDir[0]):
             print("forward")
             self.digital_write(self.enable_pin, 1)
             self.digital_write(self.dir_pin, 0)
-        else:
+        elif (Dir == MotorDir[1]):
             print("backward")
+            self.digital_write(self.enable_pin, 1)
+            self.digital_write(self.dir_pin, 1)
+        else:
+            print("the dir must be : 'forward' or 'backward'")
+            self.digital_write(self.enable_pin, 0)
+            return
+
+        self.digital_write(self.step_pin, True)
             self.digital_write(self.enable_pin, 0)
             self.digital_write(self.dir_pin, 0)
 
