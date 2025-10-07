c2bc9ff54b7100a66ab6192b38e58b4c733d1c35
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-07 08:31:05 -0400

    Update classesTest.py

--- Code/Kayden/classesTest.py
+++ Code/Kayden/classesTest.py
@@ -99,7 +99,7 @@ class HR8825():
 
     def backward(self):
         print("backward")
-        self.digital_write(self.enable_pin, 0)
+        self.digital_write(self.enable_pin, 1)
         self.digital_write(self.dir_pin, 0)
         self.digital_write(self.step_pin, True)
         def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
