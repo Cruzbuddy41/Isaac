11ee4c1be3146f2f84b64dfbb6bb9a72fde109ef
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-08 08:02:13 -0400

    Update classes_we_use.py

--- Code/classes_we_use.py
+++ Code/classes_we_use.py
@@ -70,26 +70,56 @@ class HR8825():
             print("set pins")
             self.Configure_mode(microstep[stepformat])
         
-    def TurnStep(self, Dir, steps, stepdelay=0.005):
-        if (Dir == MotorDir[0]):
-            print("forward")
-            self.digital_write(self.enable_pin, 1)
-            self.digital_write(self.dir_pin, 0)
-        elif (Dir == MotorDir[1]):
-            print("backward")
-            self.digital_write(self.enable_pin, 1)
-            self.digital_write(self.dir_pin, 1)
-        else:
-            print("the dir must be : 'forward' or 'backward'")
-            self.digital_write(self.enable_pin, 0)
-            return
+    def forward(self):
+        print("forward")
+        self.digital_write(self.enable_pin, 1)
+        self.digital_write(self.dir_pin, 1)
+        self.digital_write(self.step_pin, True)
+        def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
+            self.dir_pin = dir_pin
+            self.step_pin = step_pin        
+            self.enable_pin = enable_pin
+            self.mode_pins = mode_pins
+        
+            self.dir = GPIO.LED(self.dir_pin)
+            self.step = GPIO.LED(self.step_pin)        
+            self.enable = GPIO.LED(self.enable_pin)
+            self.mode_1 = GPIO.LED(self.mode_pins[0])
+            self.mode_2 = GPIO.LED(self.mode_pins[1])
+            self.mode_3 = GPIO.LED(self.mode_pins[2])
 
-        if (steps == 0):
-            return
-            
-        print("turn step:",steps)
-        for i in range(steps):
-            self.digital_write(self.step_pin, True)
-            time.sleep(stepdelay)
-            self.digital_write(self.step_pin, False)
-            time.sleep(stepdelay)
+            self.control_pin = {
+              dir_pin: self.dir,
+              enable_pin: self.enable,
+              step_pin: self.step,
+              mode_pins[0]: self.mode_1,
+              mode_pins[1]: self.mode_2,
+              mode_pins[2]: self.mode_3
+            }
+
+    def backward(self):
+        print("backward")
+        self.digital_write(self.enable_pin, 0)
+        self.digital_write(self.dir_pin, 0)
+        self.digital_write(self.step_pin, True)
+        def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
+            self.dir_pin = dir_pin
+            self.step_pin = step_pin        
+            self.enable_pin = enable_pin
+            self.mode_pins = mode_pins
+        
+            self.dir = GPIO.LED(self.dir_pin)
+            self.step = GPIO.LED(self.step_pin)        
+            self.enable = GPIO.LED(self.enable_pin)
+            self.mode_1 = GPIO.LED(self.mode_pins[0])
+            self.mode_2 = GPIO.LED(self.mode_pins[1])
+            self.mode_3 = GPIO.LED(self.mode_pins[2])
+
+            self.control_pin = {
+              dir_pin: self.dir,
+              enable_pin: self.enable,
+              step_pin: self.step,
+              mode_pins[0]: self.mode_1,
+              mode_pins[1]: self.mode_2,
+              mode_pins[2]: self.mode_3
+            }
