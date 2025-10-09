fec926f66eafccf8f8b9c9133996850ed080808c
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-09 08:35:36 -0400

    Update test.py

--- Code/Kayden/test.py
+++ Code/Kayden/test.py
@@ -25,8 +25,8 @@ def backward(steps):
     Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
     Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
     try:
-        Motor1.SetMicroStep('hardward', '1/16step')
-        Motor2.SetMicroStep('hardward', '1/16step')
+        Motor1.SetMicroStep('softward', '1/16step')
+        Motor2.SetMicroStep('softward', '1/16step')
         print("Motors running. Press Ctrl+C to stop.")
         time.sleep(3) # hello
         for i in range(steps):
