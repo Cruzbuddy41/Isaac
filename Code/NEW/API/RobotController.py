7fa24b592644774d5492d39cd54967fa7906bb54
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-27 08:12:02 -0400

    Update RobotController.py

--- Code/NEW/API/RobotController.py
+++ Code/NEW/API/RobotController.py
@@ -75,32 +75,3 @@ class MotorDriver():
             pwm.setDutycycle(self.PWMA, 0)
         else:
             pwm.setDutycycle(self.PWMB, 0)
-
-print("Motor driver test code")
-Motor = MotorDriver()
-
-print("Forward 5 seconds, half speed")
-Motor.MotorRun(0, 'forward', 50)
-Motor.MotorRun(1, 'forward', 50)
-time.sleep(2)
-
-print("Backward 5 seconds, half speed")
-Motor.MotorRun(0, 'backward', 50)
-Motor.MotorRun(1, 'backward', 50)
-time.sleep(2)
-
-
-print("Turn left, half speed")
-Motor.MotorRun(0, 'forward', 50)
-Motor.MotorRun(1, 'backward', 50)
-time.sleep(2.75)
-
-
-print("Turn right, half speed")
-Motor.MotorRun(0, 'backward', 50)
-Motor.MotorRun(1, 'forward', 50)
-time.sleep(3.25)
-
-print("Stop")
-Motor.MotorStop(0)
-Motor.MotorStop(1)
