fb4a10c91f8242f2c59ff7f129bdf1bae46406f0
Cruzbuddy41 <bd568711@ahschool.com>
2025-10-29 08:12:49 -0400

    Update RobotAPI.py

--- Code/NEW/API/RobotAPI.py
+++ Code/NEW/API/RobotAPI.py
@@ -33,3 +33,30 @@ async def move_forward(speed):
 async def stop():
     Motor.MotorStop()
     return {"status": "success"}
+
+
+print("Forward 5 seconds, half speed")
+Motor.MotorRun(0, 'forward', 50)
+Motor.MotorRun(1, 'forward', 50)
+time.sleep(2)
+
+print("Backward 5 seconds, half speed")
+Motor.MotorRun(0, 'backward', 50)
+Motor.MotorRun(1, 'backward', 50)
+time.sleep(2)
+
+
+print("Turn left, half speed")
+Motor.MotorRun(0, 'forward', 50)
+Motor.MotorRun(1, 'backward', 50)
+time.sleep(2.75)
+
+
+print("Turn right, half speed")
+Motor.MotorRun(0, 'backward', 50)
+Motor.MotorRun(1, 'forward', 50)
+time.sleep(3.25)
+
+print("Stop")
+Motor.MotorStop(0)
+Motor.MotorStop(1)
