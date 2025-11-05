bfe1066848cd0a25cf6cdff2d364e4b1b98882f5
bd568711 <bd568711@ahschool.com>
2025-11-05 08:20:42 -0500

    Trying auto

--- Code/API/autoFuncs.py
+++ Code/API/autoFuncs.py
@@ -1,8 +1,79 @@
 import tkinter as tk
 import requests
-import kaydensGUI
-from Code.API.kaydensGUI import move_forward
 
 base_url = "http://10.25.128.40:8000"
+
+def move_forward(speed: int, ttime: int):
+    endpoint = f"{base_url}/move/forward"
+
+    params = {
+        "speed": speed,
+        "ttime": ttime
+    }
+
+    try:
+        response = requests.post(endpoint, params=params)
+        response.raise_for_status()
+        print(f"Request successful: {response.json()}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request: {e}")
+
+def move_right(speed: int, ttime: int):
+    endpoint = f"{base_url}/move/right"
+
+    params = {
+        "speed": speed,
+        "ttime": ttime
+    }
+
+    try:
+        response = requests.post(endpoint, params=params)
+        response.raise_for_status()
+        print(f"Request successful: {response.json()}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request: {e}")
+
+def move_left(speed: int, ttime: int):
+    endpoint = f"{base_url}/move/left"
+
+    params = {
+        "speed": speed,
+        "ttime": ttime
+    }
+
+    try:
+        response = requests.post(endpoint, params=params)
+        response.raise_for_status()
+        print(f"Request successful: {response.json()}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request: {e}")
+
+def move_backward(speed: int, ttime: int):
+    endpoint = f"{base_url}/move/backward"
+
+    params = {
+        "speed": speed,
+        "ttime": ttime
+    }
+
+    try:
+        response = requests.post(endpoint, params=params)
+        response.raise_for_status()
+        print(f"Request successful: {response.json()}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request: {e}")
+
+def stop():
+    endpoint = f"{base_url}/stop"
+    try:
+        response = requests.post(endpoint)
+        response.raise_for_status()
+        print("Stop Worked")
+    except requests.exceptions.RequestException as e:
+        print("Error occured while stopping")
 
 move_forward(50, 5)
