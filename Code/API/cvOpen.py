2d2a122623301d788963a1247797eb8cec651c4f
bd568711 <bd568711@ahschool.com>
2025-11-10 08:23:11 -0500

    cv cam

--- Code/API/cvOpen.py
+++ Code/API/cvOpen.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error: Could not open video stream.")
     exit()
-
+#fff
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = 20.0
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
