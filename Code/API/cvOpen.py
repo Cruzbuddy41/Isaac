ed774e4cfa405aa7d0a5ee42aaa67ad1963c9fb2
bd568711 <bd568711@ahschool.com>
2025-11-11 08:10:04 -0500

    cv cam

--- Code/API/cvOpen.py
+++ Code/API/cvOpen.py
@@ -5,7 +5,7 @@ video_capture = cv2.VideoCapture(0)
 time.sleep(0.5) # Wait for half a second for auto-exposure
 ret, frame = video_capture.read()
 if ret:
-    cv2.imwrite('image.jpg', frame)
+    cv2.imwrite('image2.jpg', frame)
 else:
     print("Failed to capture image")
 video_capture.release()
