bca86b3afc9dff378bcba522fe7e512aae2515b9
bd568711 <bd568711@ahschool.com>
2026-02-06 08:17:11 -0500

    robot outline video

--- Code/Cam/test.py
+++ Code/Cam/test.py
@@ -1,9 +1,14 @@
+import os
+import sys
+
+# Remove the path that causes the 'xcb' error
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH", None)
+# Force the output to the Pi's HDMI monitor
+os.environ["DISPLAY"] = ":0"
+
 import cv2
 import numpy as np
 import time
-import os
-os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH", None)
-
 def nothing(x):
     pass
 
