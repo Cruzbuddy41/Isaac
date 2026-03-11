c137ab275ca81688187644a8d4fe341663c3f85f
bd568711 <bd568711@ahschool.com>
2026-03-11 08:07:35 -0400

    print which direction

--- Code/Cam/kaydensStuff (trent dont take this)/kaydensVer.py
+++ Code/Cam/kaydensStuff (trent dont take this)/kaydensVer.py
@@ -4,7 +4,7 @@ import makeLines
 
 def capture_photo_linux(filename="lanes.jpg"):
     os.system("v4l2-ctl -d /dev/video0 --set-ctrl=zoom_absolute=100")
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     for i in range(30):
