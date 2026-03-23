0b4f3bd19db4b260c6a95fbb7939405cac65ebf5
bd568711 <bd568711@ahschool.com>
2026-03-23 08:35:38 -0400

    new

---
+++ Code/API/makeLines.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+def lanes():
+
+    img = cv2.imread('lane.jpg')
+    h, w = img.shape[:2]
+
+    v1 = [int(w * 0), h - 1]  # 5% from left edge
+    v2 = [int(w * 1), h - 1]  # 95% from left edge
+    v3 = [w // 2, int(h * 0.3)]  # Middle width, 40% down from top
+
+    pts = np.array([v1, v2, v3], np.int32)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blur, 50, 150)
+
+    mask = np.zeros((h, w), dtype=np.uint8)
+    cv2.fillPoly(mask, [pts], 255)
+    masked_edges = cv2.bitwise_and(edges, mask)
+
+    lines = cv2.HoughLinesP(masked_edges, 1, np.pi / 180, threshold=50,
+                            minLineLength=100, maxLineGap=50)
+
+    mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    img = cv2.bitwise_and(img, mask_3ch)
+
+    cv2.polylines(img, [pts], isClosed=True, color=(0, 255, 0), thickness=10)
+
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
---
+++ Code/API/the_robot_photo.py
@@ -0,0 +1,53 @@
+import cv2
+import makeLines
+
+def capture_photo_linux(filename="lane.jpg"):
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    cap.set(cv2.CAP_PROP_ZOOM, 0)
+
+    if not cap.isOpened():
+        print("Could not open camera")
+        return
+
+    print("Capturing image")
+
+    for i in range(30):
+        cap.read()
+
+    ret, frame = cap.read()
+
+    cap.release()
+    if ret:
+        cv2.imwrite(filename, frame)
+        print(f"Worked (Potentially)")
+        return frame
+
+def capture_photo_mac(filename="ph.jpg"):
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_ZOOM, 0)
+    if not cap.isOpened():
+        print("Could not open camera")
+        return
+
+    print("Capturing image")
+
+    for i in range(30):
+        cap.read()
+
+    ret, frame = cap.read()
+
+    cap.release()
+    if ret:
+        cv2.imwrite(filename, frame)
+        print(f"Worked (Potentially)")
+        return frame
+
+print("Input mac for mac Input linux for linux")
+choice = input()
+
+if(choice == "mac"):
+    image = capture_photo_mac()
+else:
+    image = capture_photo_linux()
+cv2.imwrite("lane.jpg", image)
+makeLines.lanes()
