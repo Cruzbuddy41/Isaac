57450368fbdc57f768cb4fedc6462e8ad6a3edf8
bd568711 <bd568711@ahschool.com>
2026-04-01 08:09:10 -0400

    commit

--- Code/API/d4.py
+++ Code/API/d4.py
@@ -42,20 +42,20 @@ try:
         if top_pixels > turn_threshold:
             if right_pixels > left_pixels:
                 direction = "HARD LEFT"
-                movement.move_left(80, 0.1)
+                movement.move_left(80, 0.5)
             else:
                 direction = "HARD RIGHT"
-                movement.move_right(80, 0.3)
+                movement.move_right(80, 0.5)
         elif abs(pixel_diff) > correction_threshold:
             if pixel_diff > 0:
                 direction = "SLIGHT LEFT"
-                movement.move_left(60, 0.3)
+                movement.move_left(60, 0.5)
             else:
                 direction = "SLIGHT RIGHT"
-                movement.move_right(60, 0.3)
+                movement.move_right(60, 0.5)
         elif (left_pixels + right_pixels) > 500:
             direction = "FORWARD"
-            movement.move_forward(80, 0.3)
+            movement.move_forward(80, 0.5)
         else:
             direction = "SEARCHING"
 
