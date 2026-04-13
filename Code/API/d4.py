f13227cd5a80f15828ed541bdfa6efb9bb864475
deadcyberbs <bd6010870@ahschool.com>
2026-04-13 08:46:58 -0400

    adding triangle

--- Code/API/d4.py
+++ Code/API/d4.py
@@ -84,7 +84,9 @@ try:
             movement.move_left(55, 0.5)
         elif big_right_line_detected and not top_line_detected:
             direction = "FORWARD"
-            movement.move_forward(40, 0.5)
+            movement.move_forward(40, 0.75)
+            direction = "LEFT"
+            movement.move_left()
         elif top_line_detected and big_left_line_detected:
             direction = "RIGHT"
             movement.move_right(55, 0.5)
