2a39e7c9595957078f16da74f04c73aae591810c
deadcyberbs <bd6010870@ahschool.com>
2026-04-15 08:29:10 -0400

    chud detection

--- Code/API/the_robot_photo.py
+++ Code/API/the_robot_photo.py
@@ -3,12 +3,12 @@ import time
 
 
 def capture_photo_linux(filename="lane.jpg"):
-    cap = cv2.VideoCapture(1, cv2.CAP_V4L2)
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
     time.sleep(1)
 
     if not cap.isOpened():
         print("Could not open camera. Trying V4L2 backend...")
-        cap = cv2.VideoCapture(1, cv2.CAP_V4L2)
+        cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
         if not cap.isOpened():
             return None
     for i in range(30):
